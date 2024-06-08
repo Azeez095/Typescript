355cfba0ef22cba5b719bee4e7f017fb2b2ba372
AMOO AKOLADE <akoladeamoo95@gmail.com>
2024-06-08 06:30:47 +0100

    commit type

---
+++ types.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var user = { name: "Azeez", age: 28 };
+console.log(user.name);
+var greetings = "Hello World";
+console.log(greetings);
+var userId = 34465665;
+var isLoggedin = true;
--- types.ts
+++ types.ts
@@ -1,3 +1,12 @@
-let age: number = 28
-let FirstName:string = "Azeez"
-let busy: boolean = true
+let user = {name: "Azeez", age: 28}
+
+console.log(user.name)
+
+let greetings: string= "Hello World"
+console.log(greetings)
+
+let userId = 34465665;
+
+let isLoggedin:boolean= true
+
+export{}
