c36db0d144958ce10e467073f200744dcd75c92d
AMOO AKOLADE <akoladeamoo95@gmail.com>
2024-06-08 10:44:58 +0100

    Array

--- types.ts
+++ types.ts
@@ -9,4 +9,59 @@ let userId = 34465665;
 
 let isLoggedin:boolean= true
 
+function addTwo(num:number){
+    return num +2
+}
+
+ function signUp(name:string, email:string, isPaid:boolean=false) {}
+    
+ signUp("5","6@jk.com" )
+
+ const getHello = (s: string): string => {
+    return ""
+ }
+
+ function consoleerror(errmsg: string){
+    console.log(errmsg)
+ }
+
+addTwo(5)
+
+const User = {
+    name: "Azeez",
+    email: "azeez@gmail.com",
+    isActive: true
+}
+
+function createUser({name:string, isPaid:boolean}){}
+let newUser = {name: "Azeez", isPaid: "True", email: "azeez@GiMailShirt.com"}
+
+createUser(newUser)
+
+function createCourse():{}{
+    return {}
+}
+
+type User = {
+    name: string,
+    isActive: boolean
+}
+
+let myUser: User = {
+    name: "Azeez",
+    isActive: true
+}
+
+const superHeros:User[] = []
+const heroPower: number[] = []
+
+superHeros.push({name: "", isActive: false})
+heroPower.push(2)
+
+let score: number | string = 33
+
+score = 44
+score = "53"
+
+
 export{}
